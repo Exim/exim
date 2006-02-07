f767aaae1638df676a05699edf1f614cae351af2
Philip Hazel <ph10@hermes.cam.ac.uk>
2006-02-07 10:39:57 +0000

    More test files.

---
+++ test/mail/5000.new/5.myhost.test.ex
@@ -0,0 +1,12 @@
+Envelope-to: userx@myhost.test.ex
+Received: from CALLER by myhost.test.ex with local (Exim x.yz)
+	(envelope-from <CALLER@myhost.test.ex>)
+	id 10HmbD-0005vi-00
+	for userx@myhost.test.ex; Tue, 2 Mar 1999 09:44:33 +0000
+maildir:maildir_tagged_
+tag:${if eq{0}{1}{rhubarb}fail}
+Message-Id: <E10HmbD-0005vi-00@myhost.test.ex>
+From: CALLER_NAME <CALLER@myhost.test.ex>
+Date: Tue, 2 Mar 1999 09:44:33 +0000
+
+This is a test message
---
+++ test/mail/5006.userx/new/1.myhost.test.ex
@@ -0,0 +1,9 @@
+Received: from CALLER by myhost.test.ex with local (Exim x.yz)
+	(envelope-from <CALLER@test.ex>)
+	id 10HmaX-0005vi-00
+	for userx@test.ex; Tue, 2 Mar 1999 09:44:33 +0000
+Message-Id: <E10HmaX-0005vi-00@myhost.test.ex>
+From: CALLER_NAME <CALLER@test.ex>
+Date: Tue, 2 Mar 1999 09:44:33 +0000
+
+Short message
