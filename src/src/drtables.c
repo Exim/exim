Exim/exim

commit eec525c43adade97ff94d839810faf7cb35bd87f

Bug-fix for bash and no-dynamic case.
When writing the patch, originally nothing other than a cp was needed if
there were no dynamic modules.  That changed, but the guard at the top
did not.  Remove that check.

bash does not like a block which consists solely of a comment.  Provide
a ':' invocation.

Both problems spotted by Simon Arlott -- thanks.

src/scripts/lookups-Makefile
@@ -8,18 +8,14 @@ defs_source=Makefile
 tag_marker='MAGIC-TAG-MODS-OBJ-RULES-GO-HERE'
 
 tab='	'
-if grep -q "^LOOKUP.*=[ $tab]*2" "$defs_source"
-then
-  # we have work to do
-else
-  echo "No dynamic module loading support"
-  cp "$input" "$target"
-  exit 0
-fi
+
+# We always do something now, since there should always be a lookup,
+# and now we need to run in order to put the OBJ+= rules in.
 
 if grep -q "^CFLAGS_DYNAMIC[ $tab]*=" "$defs_source"
 then
   # we have a definition, we're good to go
+  : # noop (true) statement for bash compatibility
 else
   echo >&2 "Missing CFLAGS_DYNAMIC inhibits building dynamic module lookup"
   exit 1

commit 56e0c4ce946ae13628c4349f982c3407e1019dd0

Module loading working on FreeBSD (and unbreak).
(1) Commit eec525c43adade97ff94d839810faf7cb35bd87f broke module
    support, because we *do* still need some exported variable
    definitions, as for a module to actually work, we need the
    per-module _INCLUDE/_LIBS settings.

(2) FreeBSD's nsdispatch() will leave dlerror() returning a complaint
    about "_nss_cache_cycle_prevention_function"; we need to clear the
    error state before the dlsym() call, so that any error found
    afterwards must have come from that dlsym() call.  Fix is just an
    extra call to dlerror(), which should be portable.

I can now use sqlite3 from a module, in FreeBSD.

src/scripts/lookups-Makefile
@@ -47,6 +47,7 @@ emit_module_rule() {
   if want_dynamic "$lookup_name"
   then
     echo "MODS += ${mod_name}.so"
+    grep "^LOOKUP_${lookup_name}_" "$defs_source"
     echo "LOOKUP_${mod_name}_INCLUDE = \$(LOOKUP_${lookup_name}_INCLUDE)"
     echo "LOOKUP_${mod_name}_LIBS = \$(LOOKUP_${lookup_name}_LIBS)"
   elif want_at_all "$lookup_name"

src/src/drtables.c
@@ -518,6 +518,13 @@ extern lookup_module_info whoson_lookup_module_info;
           continue;
         }
 
+        /* FreeBSD nsdispatch() can trigger dlerror() errors about
+         * _nss_cache_cycle_prevention_function; we need to clear the dlerror()
+         * state before calling dlsym(), so that any error afterwards only
+         * comes from dlsym().
+         */
+        errormsg = dlerror();
+
         info = (struct lookup_module_info*) dlsym(dl, "_lookup_module_info");
         if ((errormsg = dlerror()) != NULL) {
           fprintf(stderr, "%s does not appear to be a lookup module (%s)\n", name, errormsg);