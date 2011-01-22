0b4f0dad5075fa5a76dde9b677982fa565731b51
Phil Pennock <pdp@exim.org>
2011-01-22 21:52:17 +0000

    Sign Script - Take EXIM_KEY from environ.

--- release-process/scripts/sign_exim_packages.sh
+++ release-process/scripts/sign_exim_packages.sh
@@ -3,14 +3,15 @@
 # $Cambridge: exim/release-process/scripts/sign_exim_packages.sh,v 1.1 2010/06/03 12:00:38 nm4 Exp $
 #
 # gpg signs the package set.
-# key used is currently coded into the script
+# key used set from env var EXIM_KEY, script defaults that to Nigel's.
 # woe betide the poor sod who does not use a gpg agent, so has
 # to enter their password for every file...
 #
-exim_key='nigel@exim.org'
+
+: ${EXIM_KEY:=nigel@exim.org}
 
 
 for file in *.tar.gz *.tar.bz2
 do
-  gpg  --local-user ${exim_key} --detach-sig --armor $file
+  gpg  --local-user ${EXIM_KEY} --detach-sig --armor $file
 done
