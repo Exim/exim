e0fccd1ddccf02568e9f8e6af38e62513f306fe5
Tony Finch <dot@dotat.at>
2012-12-07 11:52:09 +0000

    A safer version of the check for gecos expansion.

--- src/src/receive.c
+++ src/src/receive.c
@@ -2039,8 +2039,8 @@ for (h = header_list->next; h != NULL; h = h->next)
         uschar *s = Ustrchr(h->text, ':') + 1;
         while (isspace(*s)) s++;
         len = h->slen - (s - h->text) - 1;
-        if (strncmpic(s, originator_login, len) == 0
-            && Ustrlen(originator_login) == len)
+        if (Ustrlen(originator_login) == len &&
+	    strncmpic(s, originator_login, len) == 0)
           {
           uschar *name = is_resent? US"Resent-From" : US"From";
           header_add(htype_from, "%s: %s <%s@%s>\n", name, originator_name,
