d84ab8444e381f386e9d4e67360302aa13459540
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2016-02-29 21:21:43 +0100

    Avoid warnings about constness
    For recent versions these warnings disappear, because lots of
    functions are constified. Here we hot-fix it just for the backporters
    compiler.

--- src/src/environment.c
+++ src/src/environment.c
@@ -31,7 +31,7 @@ else if (Ustrcmp(keep_environment, "*") != 0)
     {
     uschar *name = string_copyn(*p, US Ustrchr(*p, '=') - *p);
 
-    if (OK != match_isinlist(name, CUSS &keep_environment,
+    if (OK != match_isinlist(name, USS &keep_environment,
         0, NULL, NULL, MCL_NOEXPAND, FALSE, NULL))
       if (unsetenv(CS name) < 0) return FALSE;
       else /* nothing */;
@@ -45,7 +45,7 @@ if (add_environment)
   {
     uschar *p;
     int sep = 0;
-    const uschar* envlist = add_environment;
+    uschar* envlist = add_environment;
     while ((p = string_nextinlist(&envlist, &sep, NULL, 0)))
         putenv(CS p);
   }
