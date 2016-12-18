3c1ae30be64af20d22f7a3308b8a8d2a15c01235
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2016-12-18 23:35:52 +0100

    Testsuite: backported 4510 (DKIM information leak)

---
+++ src/config.h
@@ -0,0 +1,22 @@
+/*************************************************
+*           Configuration header for Exim        *
+*************************************************/
+
+/* This file was automatically generated from Makefile and config.h.defaults,
+using values specified in the configuration file Local/Makefile.
+Do not edit it. Instead, edit Local/Makefile and rerun make. */
+
+#ifndef OFF_T_FMT
+#define OFF_T_FMT  "%ld"
+#define LONGLONG_T long int
+#endif
+
+#ifndef TIME_T_FMT
+#define TIME_T_FMT  "%ld"
+#endif
+
+#define SIZE_T_FMT  "%lu"
+#define SSIZE_T_FMT  "%ld"
+#define HAVE_IPV6             FALSE
+#define HAVE_ICONV            FALSE
+
