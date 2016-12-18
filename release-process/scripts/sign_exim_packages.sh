e86c38b0ecfb07b1788e81b7dd13d3adcadce424
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2016-12-18 10:23:47 +0100

    Release process: sign all *.tar.* under a given dir

--- release-process/scripts/sign_exim_packages.sh
+++ release-process/scripts/sign_exim_packages.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #
-# gpg signs the package set.
+# gpg signs all *.tar.* files under a given directory
 # key used set from env var EXIM_KEY, script defaults that to Nigel's.
 # woe betide the poor sod who does not use a gpg agent, so has
 # to enter their password for every file...
 #
 
+dir=${1:?start directory}
+
 : ${EXIM_KEY:=nigel@exim.org}
 
-for file in *.tar.gz *.tar.bz2
-do
-  gpg  --local-user ${EXIM_KEY} --detach-sig --armor $file
-done
+find "$dir" \
+    -type f -name '*.tar.*' \
+    -exec gpg  --local-user ${EXIM_KEY} --detach-sig --armor {} \;
