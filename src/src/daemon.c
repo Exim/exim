6461b5e2e8ec214923ce088507be969e3116c1fe
Josh Soref <jsoref@users.noreply.github.com>
2017-01-16 04:58:23 +0000

    spelling: explicit

--- src/src/daemon.c
+++ src/src/daemon.c
@@ -1117,7 +1117,7 @@ if (daemon_listen && !inetd_wait_mode)
     }
 
   /* Create a list of default SMTP ports, to be used if local_interfaces
-  contains entries without explict ports. First count the number of ports, then
+  contains entries without explicit ports. First count the number of ports, then
   build a translated list in a vector. */
 
   list = daemon_smtp_port;
