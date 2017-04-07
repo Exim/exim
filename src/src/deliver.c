5474322b42fbdde2c9620a05d8ea2abe24524109
Alexandru Chirila <alex@alexkiro.com>
2017-04-07 18:15:51 +0300

    Add Diagnostic-Code for unroutable address
    https://bugs.exim.org/show_bug.cgi?id=2013

--- src/src/deliver.c
+++ src/src/deliver.c
@@ -5290,18 +5290,27 @@ Returns:       nothing
 static void
 print_dsn_diagnostic_code(const address_item *addr, FILE *f)
 {
-uschar *s = testflag(addr, af_pass_message) ? addr->message : NULL;
+uschar *s;
+DEBUG(D_deliver)
+  debug_printf("DSN Diagnostic-Code: pass_message=%s, message=%s, user_message=%s\n", testflag(addr, af_pass_message), addr->message, addr->user_message);
 
 /* af_pass_message and addr->message set ? print remote host answer */
+/* search first ": ". we assume to find the remote-MTA answer there */
+if (testflag(addr, af_pass_message) && addr->message && Ustrstr(addr->message, ": "))
+  {
+  s = addr->message;
+  s = Ustrstr(addr->message, ": ");
+  s += 2;
+  }
+else
+  {
+  s = addr->user_message;
+  }
+DEBUG(D_deliver)
+  debug_printf("DSN Diagnostic-Code: %s\n", s);
+
 if (s)
   {
-  DEBUG(D_deliver)
-    debug_printf("DSN Diagnostic-Code: addr->message = %s\n", addr->message);
-
-  /* search first ": ". we assume to find the remote-MTA answer there */
-  if (!(s = Ustrstr(addr->message, ": ")))
-    return;				/* not found, bail out */
-  s += 2;  /* skip ": " */
   fprintf(f, "Diagnostic-Code: smtp; ");
   }
 /* no message available. do nothing */
@@ -7601,6 +7610,7 @@ wording. */
           addr->next = handled_addr;
           handled_addr = topaddr;
           }
+       print_dsn_diagnostic_code(addr, f);
 	fputc('\n', f);
         }
 
