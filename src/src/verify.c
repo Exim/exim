5ea5d64c99a3f99722034f11d7bf79ab9d10f014
Jeremy Harris <jgh146exb@wizmail.org>
2017-04-26 20:18:31 +0100

    Tweak debug output

--- src/src/verify.c
+++ src/src/verify.c
@@ -1395,6 +1395,7 @@ cutthrough.delivery = cutthrough.callout_hold_only = FALSE;
 void
 release_cutthrough_connection(const uschar * why)
 {
+if (cutthrough.fd < 0) return;
 HDEBUG(D_acl) debug_printf_indent("release cutthrough conn: %s\n", why);
 cutthrough.fd = -1;
 cutthrough.delivery = cutthrough.callout_hold_only = FALSE;
