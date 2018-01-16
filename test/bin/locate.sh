fc8a300f49e9457ada1834ae2feda738784f73c6
Jeremy Harris <jgh146exb@wizmail.org>
2018-01-16 23:14:49 +0000

    Testsuite: MySQL portability
    Community-mysql has no mysqld-safe script, and mysqld has varying locations.

--- test/bin/locate.sh
+++ test/bin/locate.sh
@@ -3,23 +3,30 @@
 [ -d bin.sys ] || mkdir bin.sys
 cd bin.sys
 
-while read d
+while [ $# -gt 0 ]
 do
-  if [ -x $d/$1 ]
-  then
-    while [ $# -gt 0 ]
-    do
+  while read d
+  do
+    if [ -x $d/$1 ]
+    then
       rm -f ./$1
       ln -s $d/$1 .
-      shift
-    done
-    exit 0
-  fi
-done <<-HERE
+      break
+    fi
+  done <<-HERE
 	/bin
 	/usr/bin
+	/usr/sbin
+	/usr/libexec
 	/usr/local/bin
 	/usr/lib/postgresql/10/bin
+	/usr/lib/postgresql/9.5/bin
+	/usr/lib/postgresql/9.4/bin
+	/usr/lib/postgresql/9.3/bin
+	/usr/lib/postgresql/9.2/bin
+	/usr/lib/postgresql/9.1/bin
 	/usr/lib/postgresql/9/bin
 HERE
+  shift
+done
 
