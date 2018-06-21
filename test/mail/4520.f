2fc30c289769075598c1c5c536dfd74f2bafbb11
Jeremy Harris <jgh146exb@wizmail.org>
2018-06-21 18:22:56 +0100

    Testsuite: missing output files

---
+++ test/mail/4520.f
@@ -0,0 +1,22 @@
+From CALLER@myhost.test.ex Tue Mar 02 09:44:33 1999
+Received: from the.local.host.name ([ip4.ip4.ip4.ip4] helo=myhost.test.ex)
+	by myhost.test.ex with esmtp (Exim x.yz)
+	(envelope-from <CALLER@myhost.test.ex>)
+	id 10HmbO-0005vi-00
+	for f@test.ex; Tue, 2 Mar 1999 09:44:33 +0000
+DKIM-Signature: v=1; a=rsa-sha256; q=dns/txt; c=relaxed/relaxed; d=test.ex;
+	s=sel; h=From; bh=gs+F4Uhr+N4qZhWyfctULkCwprl/tvOnzqNhsmpAFW4=; b=A3l0nyvB3OK
+	NuIDfQJg0wynUUH4AWSRdDUNmhkTNo2RW/ifLCWBky6AXfkaHENJRewncjoVcn92d0cbTF4gDEhUi
+	EbuJRdYja2OJT25BUgLfrxQnNnYB7TB9PcWG3Mrmq0fGRgCNngyh5gLHGqu9tBllw5Pdigbq2EAV0
+	t2W/iI=;
+Received: from CALLER by myhost.test.ex with local (Exim x.yz)
+	(envelope-from <CALLER@myhost.test.ex>)
+	id 10HmbN-0005vi-00
+	for f@test.ex; Tue, 2 Mar 1999 09:44:33 +0000
+From: nobody@example.com
+Message-Id: <E10HmbN-0005vi-00@myhost.test.ex>
+Sender: CALLER_NAME <CALLER@myhost.test.ex>
+Date: Tue, 2 Mar 1999 09:44:33 +0000
+
+..content
+
