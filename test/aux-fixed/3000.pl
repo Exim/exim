00ebeb1f7a6480c83e2ce4f047bd6ad5b9d456d7
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2018-10-26 22:04:24 +0200

    testsuite: diet for aux-fixed/3000.pl

--- test/aux-fixed/3000.pl
+++ test/aux-fixed/3000.pl
@@ -1,27 +1,27 @@
 use POSIX qw(locale_h);
 
-sub foo { "Subroutine foo called with args: @_"; }
+sub foo { "Subroutine foo called with args: @_" }
 
-sub foo_undef { $x; }
+sub foo_undef { undef }
 
-sub foo_die { die "expiring..."; }
+sub foo_die { die 'expiring...' }
 
-sub readvar { Exim::expand_string("\$$_[0]"); }
+sub readvar { Exim::expand_string("\$$_[0]") }
 
-sub return_scalar { 42; }
+sub return_scalar { 42 }
 
-sub return_list { (10,20,30); }
+sub return_list { (10, 20, 30) }
 
-sub return_variable_vector { @x = (4,5,6); @x; }
+sub return_variable_vector { @x = (4, 5, 6) }
 
-sub return_hash { %x = ("a", 4, "b", 5); %x; }
+sub return_hash { (a => 4, b => 5) }
 
-sub debug_write { Exim::debug_write("$_[0]"); "Wrote debug"; }
+sub debug_write { Exim::debug_write($_[0]); 'Wrote debug' }
 
-sub log_write { Exim::log_write("$_[0]"); "Wrote log"; }
+sub log_write { Exim::log_write($_[0]); 'Wrote log' }
 
-sub change_locale { setlocale(LC_TIME, "fr_FR"); "Changed locale"; }
+sub change_locale { setlocale(LC_TIME, 'fr_FR'); 'Changed locale' }
 
-sub foo_warn { warn "this is a warning"; "Wrote warning"; }
+sub foo_warn { warn 'this is a warning'; 'Wrote warning' }
 
-sub no_warn { $SIG{__WARN__} = sub { }; "Discarded warnings"; }
+sub no_warn { $SIG{__WARN__} = sub { }; 'Discarded warnings' }
