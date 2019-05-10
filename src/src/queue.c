e5903596a0f6b078f5854ca591963e21c7f7328a
Jeremy Harris <jgh146exb@wizmail.org>
2019-05-10 15:18:56 +0100

    Fix listing a named queue by a non-admin user.  Bug 2398

--- src/src/queue.c
+++ src/src/queue.c
@@ -778,11 +778,12 @@ Argument:    points to the tree node
 Returns:     nothing
 */
 
-static void queue_list_extras(tree_node *p)
+static void
+queue_list_extras(tree_node *p)
 {
-if (p->left != NULL) queue_list_extras(p->left);
+if (p->left) queue_list_extras(p->left);
 if (!p->data.val) printf("       +D %s\n", p->name);
-if (p->right != NULL) queue_list_extras(p->right);
+if (p->right) queue_list_extras(p->right);
 }
 
 
