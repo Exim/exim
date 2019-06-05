171adf11d72efb4781a3028a849e0ed6e521a4fa
Phil Pennock <pdp@exim.org>
2019-06-05 05:35:28 -0400

    Unbreak heimdal_gssapi auth driver
    Commit 251b9eb46 broke heimdal_gssapi by changing the function
    definition in the `.c` without changing the declaration in the `.h`.
    Was part of 4.92.
    
    Make corresponding `.h` change to reflect newer internal API.
    
    (cherry picked from commit 6ee110613402e8562c03b4a11c3ffbdbd47bd153)
    (cherry picked from commit 40fe3ea73eb7524a6143755854633ed8392d39b4)

--- src/src/auths/heimdal_gssapi.h
+++ src/src/auths/heimdal_gssapi.h
@@ -32,8 +32,7 @@ extern auth_heimdal_gssapi_options_block auth_heimdal_gssapi_option_defaults;
 
 extern void auth_heimdal_gssapi_init(auth_instance *);
 extern int auth_heimdal_gssapi_server(auth_instance *, uschar *);
-extern int auth_heimdal_gssapi_client(auth_instance *, smtp_inblock *,
-                                smtp_outblock *, int, uschar *, int);
+extern int auth_heimdal_gssapi_client(auth_instance *, void *, int, uschar *, int);
 extern void auth_heimdal_gssapi_version_report(FILE *f);
 
 /* End of heimdal_gssapi.h */
