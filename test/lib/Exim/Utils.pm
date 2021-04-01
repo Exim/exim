47bc7a521803eaea0d6666c327f2e21e22eb920b
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2021-04-01 11:33:01 +0200

    testsuite: provide cp() if File::Copy is too old.

--- test/lib/Exim/Utils.pm
+++ test/lib/Exim/Utils.pm
@@ -2,8 +2,9 @@ package Exim::Utils;
 use v5.10.1;
 use strict;
 use warnings;
+use File::Copy;
 use parent 'Exporter';
-our @EXPORT_OK = qw(uniq numerically);
+our @EXPORT_OK = qw(uniq numerically cp);
 
 
 sub uniq {
@@ -12,5 +13,15 @@ sub uniq {
 }
 
 sub numerically { $::a <=> $::b }
+
+sub cp {
+    if ($File::Copy::VERSION >= 2.15) { # since Perl 5.11 we should have >= 2.15
+        return File::Copy::cp(@_);
+    }
+    copy(@_) or return undef;
+    my ($src, $dst) = @_;
+    my @st = stat $src or return undef;
+    chmod($st[2]&07777, $dst);
+}
 
 1;
