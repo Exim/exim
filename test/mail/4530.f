15a44d749b2f4097d43c2d887b6c5bca2d0d8b4a
Jeremy Harris <jgh146exb@wizmail.org>
2021-08-10 17:36:03 +0100

    Testsuite: testcases for DKIM under TLS

---
+++ test/mail/4530.f
@@ -0,0 +1,25 @@
+From CALLER@myhost.test.ex Tue Mar 02 09:44:33 1999
+Received: from the.local.host.name ([ip4.ip4.ip4.ip4] helo=myhost.test.ex)
+	by myhost.test.ex with esmtps (TLS1.x:ke-RSA-AES256-SHAnnn:xxx)
+	(Exim x.yz)
+	(envelope-from <CALLER@myhost.test.ex>)
+	id 10HmbP-0005vi-00
+	for f@test.ex;
+	Tue, 2 Mar 1999 09:44:33 +0000
+DKIM-Signature: v=1; a=rsa-sha256; q=dns/txt; c=relaxed/relaxed; d=test.ex;
+	s=sel; h=From; bh=gs+F4Uhr+N4qZhWyfctULkCwprl/tvOnzqNhsmpAFW4=; b=A3l0nyvB3OK
+	NuIDfQJg0wynUUH4AWSRdDUNmhkTNo2RW/ifLCWBky6AXfkaHENJRewncjoVcn92d0cbTF4gDEhUi
+	EbuJRdYja2OJT25BUgLfrxQnNnYB7TB9PcWG3Mrmq0fGRgCNngyh5gLHGqu9tBllw5Pdigbq2EAV0
+	t2W/iI=;
+Received: from CALLER by myhost.test.ex with local (Exim x.yz)
+	(envelope-from <CALLER@myhost.test.ex>)
+	id 10HmbO-0005vi-00
+	for f@test.ex;
+	Tue, 2 Mar 1999 09:44:33 +0000
+From: nobody@example.com
+Message-Id: <E10HmbO-0005vi-00@myhost.test.ex>
+Sender: CALLER_NAME <CALLER@myhost.test.ex>
+Date: Tue, 2 Mar 1999 09:44:33 +0000
+
+..content
+
