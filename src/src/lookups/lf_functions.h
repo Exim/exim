95dcd84413ec656abfd8fcd46c8db0ce11926f24
Martin D Kealey <martin@kurahaupo.gen.nz>
2022-09-22 19:04:29 +1000

    Use macro args exactly once

--- src/src/lookups/lf_functions.h
+++ src/src/lookups/lf_functions.h
@@ -19,45 +19,54 @@ extern int     lf_sqlperform(const uschar *, const uschar *, const uschar *,
                  uschar **, BOOL *, uint *, const uschar *));
 
 /*
- * The st_mode field returned by stat() indicates the type of a file; these
- * functions help to interpret that.
- *
- * Usage:
- *  struct stat s;
- *  const uschar *t;
- *  stat(&s, filename);
- *  if ((t = S_IFMT_to_long_name(s.st_mode)))
- *    printf("%s is a %s\n", filename, t);
- *  else
- *    printf("%s is unknown\n", filename);
- *  if ( allowed_filetype_mask & BIT(S_IFMT_to_index(s.st_mode)) )
- *    printf("\tALLOWED\n");
- *  else
- *    printf("\tFORBIDDEN\n");
- *
- * S_IFMT_to_index shifts this to remove bits that are not part of S_IFMT,
- * making a "small" number suitable for an array index; S_IFMT_from_index does
- * the reverse. These indeces can be used with the S_IFMTix_* variants; the
- * others use the mode masked with S_IFMT directly.
- *
- * The _ucname and _name versions are the same, differing only in case; the
- * _long_name versions provide human-readable forms suitable for logging.
- *
- * Macros for use in static initialisers; take care to avoid side effects in
- * parameters
+  The st_mode field returned by stat() indicates the type of a file; these
+  functions help to interpret that.
+
+  Usage:
+   struct stat s;
+   const uschar *t;
+   stat(&s, filename);
+   if ((t = S_IFMT_to_long_name(s.st_mode)))
+     printf("%s is a %s\n", filename, t);
+   else
+     printf("%s is unknown\n", filename);
+   if ( allowed_filetype_mask & BIT(S_IFMT_to_index(s.st_mode)) )
+     printf("\tALLOWED\n");
+   else
+     printf("\tFORBIDDEN\n");
+
+  S_IFMT_to_index shifts this to remove bits that are not part of S_IFMT,
+  making a "small" number suitable for an array index; S_IFMT_from_index does
+  the reverse. These indeces can be used with the S_IFMTix_* variants; the
+  others use the mode masked with S_IFMT directly.
+
+  The _ucname and _name versions are the same, differing only in case; the
+  _long_name versions provide human-readable forms suitable for logging.
  */
-#define S_IFMT_scale (S_IFMT & -S_IFMT)  /* X&-X comprises the least significant 1-bit of X, all other bits 0 */
-#define S_IFMT_to_index(S)   ( (S)<0 ? -1 : (S_IFMT & (S)) / S_IFMT_scale )
-#define S_IFMT_from_index(I) ( (I)<0 ? -1 :  S_IFMT & (I)  * S_IFMT_scale )
 
-extern const uschar *S_IFMTix_to_name(int index);      /* NULL on error */
-extern const uschar *S_IFMTix_to_long_name(int index); /* NULL on error */
-extern const uschar *S_IFMTix_to_ucname(int index);    /* NULL on error */
-extern int S_IFMTix_from_name(const uschar *name);     /* negative on error */
+/* In all traditional Unix systems S_IFMT == 0xf000, so S_IFMT_scale == 0x1000
+   == (1<<12); however this method will work with any other "reasonable"
+   values, in particular any positive value for S_IFMT, provided
+   ( 1 << S_IFMT / S_IFMT_scale ) fits inside an unsigned int without rollover - in
+   general, so it spans no more than 5 bits.
+*/
+#define S_IFMT_scale (S_IFMT & -S_IFMT)  /* X&-X computes the greatest power of 2 that divides into X */
+/* static_assert( S_IFMT > 0 && S_IFMT / S_IFMT_scale < 32 ); */
 
-static inline const uschar *S_IFMT_to_name(int index)      { return S_IFMTix_to_name(     S_IFMT_to_index(index));   } /* NULL on error */
-static inline const uschar *S_IFMT_to_long_name(int index) { return S_IFMTix_to_long_name(S_IFMT_to_index(index));   } /* NULL on error */
-static inline const uschar *S_IFMT_to_ucname(int index)    { return S_IFMTix_to_ucname(   S_IFMT_to_index(index));   } /* NULL on error */
-static inline int S_IFMT_from_name(const uschar *name)     { return S_IFMT_from_index(    S_IFMTix_from_name(name)); } /* negative on error */
+/* These need to be macros (rather than functions) to allow them to be used in
+   static initialisers */
+#define S_IFMT_to_index(S)   ( (S_IFMT & (S)) / S_IFMT_scale )
+#define S_IFMT_from_index(I) (  S_IFMT & (I)  * S_IFMT_scale )
+
+
+extern const uschar *S_IFMTix_to_name(int index);       /* NULL on error */
+extern const uschar *S_IFMTix_to_long_name(int index);  /* NULL on error */
+extern const uschar *S_IFMTix_to_ucname(int index);     /* NULL on error */
+extern int S_IFMTix_from_name(const uschar *name);      /* negative on error */
+
+static inline const uschar *S_IFMT_to_name(int index)      { int i = S_IFMT_to_index(index);   return i<0 ? NULL : S_IFMTix_to_name(i);      } /* NULL on error */
+static inline const uschar *S_IFMT_to_long_name(int index) { int i = S_IFMT_to_index(index);   return i<0 ? NULL : S_IFMTix_to_long_name(i); } /* NULL on error */
+static inline const uschar *S_IFMT_to_ucname(int index)    { int i = S_IFMT_to_index(index);   return i<0 ? NULL : S_IFMTix_to_ucname(i);    } /* NULL on error */
+static inline int S_IFMT_from_name(const uschar *name)     { int i = S_IFMTix_from_name(name); return i<0 ? -1   : S_IFMT_from_index(i);     } /* negative on error */
 
 /* End of lf_functions.h */
