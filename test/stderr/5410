Exim/exim

commit 90bd3832bc0ff090ac5e37dfc66b30cabb9cfc1a

Debug: option access for expansion

test/stderr/0092
@@ -54,13 +54,18 @@ configuration file is TESTSUITE/test-config
 admin user
 changed uid/gid: privilege not needed
   uid=EXIM_UID gid=EXIM_GID pid=p1234
+try option gecos_pattern
+try option gecos_name
+try option unknown_login
 originator: uid=CALLER_UID gid=CALLER_GID login=CALLER name=CALLER_NAME
 sender address = CALLER@myhost.test.ex
+try option smtp_active_hostname
 sender_fullhost = [V4NET.0.0.1]
 sender_rcvhost = [V4NET.0.0.1]
 host in hosts_connection_nolog? no (option unset)
 LOG: smtp_connection MAIN
   SMTP connection from [V4NET.0.0.1]
+try option message_size_limit
 host in host_lookup? no (option unset)
 set_process_info: pppp handling incoming connection from [V4NET.0.0.1]
  ╭considering: ${if eq {V4NET.0.0.1} {$sender_host_address} {2} {30}}s
@@ -98,6 +103,8 @@ host in recipient_unqualified_hosts? no (option unset)
 host in helo_verify_hosts? no (option unset)
 host in helo_try_verify_hosts? no (option unset)
 host in helo_accept_junk_hosts? no (option unset)
+try option acl_smtp_connect
+try option smtp_banner
  ╭considering: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ├──────value: myhost.test.ex
  ├considering:  ESMTP Exim $version_number $tod_full
@@ -120,12 +127,16 @@ test in helo_lookup_domains? no (end of list)
 sender_fullhost = (test) [V4NET.0.0.1]
 sender_rcvhost = [V4NET.0.0.1] (helo=test)
 set_process_info: pppp handling incoming connection from (test) [V4NET.0.0.1]
+try option spf_smtp_comment_template
+try option acl_smtp_helo
 SMTP>> 250 myhost.test.ex Hello test [V4NET.0.0.1]
 SMTP<< mail from:userx@test.ex
 spool directory space = nnnnnK inodes = nnnnn check_space = 10240K inodes = 100 msg_size = 0
 log directory space = nnnnnK inodes = nnnnn check_space = 10240K inodes = 100
+try option acl_smtp_mail
 SMTP>> 250 OK
 SMTP<< rcpt to:userx@test.ex
+try option acl_smtp_rcpt
 using ACL "check_recipient"
 processing "accept" (TESTSUITE/test-config 27)
 check hosts = :
@@ -156,10 +167,12 @@ accept: condition test succeeded in ACL "check_recipient"
 end of ACL "check_recipient": ACCEPT
 SMTP>> 250 Accepted
 SMTP<< data
+try option acl_smtp_predata
 SMTP>> 354 Enter message, ending with "." on a line by itself
 search_tidyup called
 LOG: lost_incoming_connection MAIN
   SMTP data timeout (message abandoned) on connection from (test) [V4NET.0.0.1] F=<userx@test.ex> D=qqs
+try option acl_smtp_notquit
 SMTP>> 421 myhost.test.ex SMTP incoming data timeout - closing connection.
 search_tidyup called
 >>>>>>>>>>>>>>>> Exim pid=p1234 (fresh-exec) terminating with rc=1 >>>>>>>>>>>>>>>>

test/stderr/0402
@@ -14,11 +14,16 @@ seeking password data for user "CALLER": using cached result
 getpwnam() succeeded uid=CALLER_UID gid=CALLER_GID
 seeking password data for user "CALLER": using cached result
 getpwnam() succeeded uid=CALLER_UID gid=CALLER_GID
+try option gecos_pattern
+try option gecos_name
+try option unknown_login
 originator: uid=CALLER_UID gid=CALLER_GID login=CALLER name=CALLER_NAME
 sender address = CALLER@test.ex
+try option smtp_active_hostname
 set_process_info: pppp accepting a local non-SMTP message from <CALLER@test.ex>
 spool directory space = nnnnnK inodes = nnnnn check_space = 10240K inodes = 100 msg_size = 0
 log directory space = nnnnnK inodes = nnnnn check_space = 10240K inodes = 100
+try option message_size_limit
 Sender: CALLER@test.ex
 Recipients:
   CALLER@test.ex
@@ -26,9 +31,12 @@ Recipients:
   userz
   rd+CALLER
   rd+usery
+try option acl_not_smtp_start
 search_tidyup called
 >>Headers received:
 
+try option message_id_header_domain
+try option message_id_header_text
 qualify & rewrite recipients list
 rewrite rules on sender address
 qualify and rewrite headers
@@ -45,6 +53,7 @@ Data file written for message 10HmaX-000000005vi-0000
  ╭considering: ${tod_full}
  ├──expanding: ${tod_full}
  ╰─────result: Tue, 2 Mar 1999 09:44:33 +0000
+try option received_header_text
  ╭considering: Received: ${if def:sender_rcvhost {from $sender_rcvhost
  	}{${if def:sender_ident {from ${quote_local_part:$sender_ident} }}${if def:sender_helo_name {(helo=$sender_helo_name)
  	}}}}by $primary_hostname ${if def:received_protocol {with $received_protocol }}${if def:tls_in_ver        { ($tls_in_ver)}}${if def:tls_in_cipher_std { tls $tls_in_cipher_std
@@ -380,6 +389,8 @@ P Received: from CALLER by mail.test.ex with local (Exim x.yz)
 	(envelope-from <CALLER@test.ex>)
 	id 10HmaX-000000005vi-0000;
 	Tue, 2 Mar 1999 09:44:33 +0000
+try option acl_not_smtp_mime
+try option acl_not_smtp
  ╭considering: ${tod_full}
  ├──expanding: ${tod_full}
  ╰─────result: Tue, 2 Mar 1999 09:44:33 +0000
@@ -513,6 +524,7 @@ checking local_parts
 usery in "usery"?
  list element: usery
  usery in "usery"? yes (matched "usery")
+try option router_home_directory
  ╭considering: /non-exist/$domain
  ├───────text: /non-exist/
  ├considering: $domain
@@ -521,17 +533,23 @@ usery in "usery"?
  ├──expanding: /non-exist/$domain
  ╰─────result: /non-exist/test.ex
             ╰──(tainted)
+try option set
 calling r5 router
+try option qualify_domain
 rda_interpret (string): 'TESTSUITE/test-mail/junk'
 expanded: 'TESTSUITE/test-mail/junk'
 file is not a filter file
 parse_forward_list: TESTSUITE/test-mail/junk
 extract item: TESTSUITE/test-mail/junk
+try option file_transport
+try option transport
 set transport ft1
 r5 router generated TESTSUITE/test-mail/junk
   pipe, file, or autoreply
   errors_to=NULL transport=ft1
   uid=unset gid=unset home=/non-exist/$local_part
+try option unseen
+try option unseen
 routed by r5 router
   envelope to: rd+usery@test.ex
   transport: <none>
@@ -565,6 +583,7 @@ checking local_parts
 CALLER in "CALLER"?
  list element: CALLER
  CALLER in "CALLER"? yes (matched "CALLER")
+try option router_home_directory
  ╭considering: /non-exist/$local_part
  ├───────text: /non-exist/
  ├considering: $local_part
@@ -573,17 +592,23 @@ CALLER in "CALLER"?
  ├──expanding: /non-exist/$local_part
  ╰─────result: /non-exist/CALLER
             ╰──(tainted)
+try option set
 calling r4 router
+try option qualify_domain
 rda_interpret (string): 'TESTSUITE/test-mail/junk'
 expanded: 'TESTSUITE/test-mail/junk'
 file is not a filter file
 parse_forward_list: TESTSUITE/test-mail/junk
 extract item: TESTSUITE/test-mail/junk
+try option file_transport
+try option transport
 set transport ft1
 r4 router generated TESTSUITE/test-mail/junk
   pipe, file, or autoreply
   errors_to=NULL transport=ft1
   uid=unset gid=unset home=/non-exist/CALLER
+try option unseen
+try option unseen
 routed by r4 router
   envelope to: rd+CALLER@test.ex
   transport: <none>
@@ -609,6 +634,7 @@ checking local_parts
 userz in "userz"?
  list element: userz
  userz in "userz"? yes (matched "userz")
+try option router_home_directory
  ╭considering: /non-exist/$domain
  ├───────text: /non-exist/
  ├considering: $domain
@@ -617,14 +643,18 @@ userz in "userz"?
  ├──expanding: /non-exist/$domain
  ╰─────result: /non-exist/test.ex
             ╰──(tainted)
+try option set
 calling r3 router
 r3 router called for userz@test.ex
   domain = test.ex
+try option transport
 set transport t2
 queued for t2 transport: local_part = userz
 domain = test.ex
   errors_to=NULL
   domain_data=NULL local_part_data=userz
+try option unseen
+try option unseen
 routed by r3 router
   envelope to: userz@test.ex
   transport: t2
@@ -643,6 +673,7 @@ checking local_parts
 usery in "usery"?
  list element: usery
  usery in "usery"? yes (matched "usery")
+try option router_home_directory
  ╭considering: /non-exist/$domain
  ├───────text: /non-exist/
  ├considering: $domain
@@ -651,14 +682,18 @@ usery in "usery"?
  ├──expanding: /non-exist/$domain
  ╰─────result: /non-exist/test.ex
             ╰──(tainted)
+try option set
 calling r2 router
 r2 router called for usery@test.ex
   domain = test.ex
+try option transport
 set transport t1
 queued for t1 transport: local_part = usery
 domain = test.ex
   errors_to=NULL
   domain_data=NULL local_part_data=usery
+try option unseen
+try option unseen
 routed by r2 router
   envelope to: usery@test.ex
   transport: t1
@@ -670,6 +705,7 @@ checking local_parts
 CALLER in "CALLER"?
  list element: CALLER
  CALLER in "CALLER"? yes (matched "CALLER")
+try option router_home_directory
  ╭considering: /non-exist/$local_part
  ├───────text: /non-exist/
  ├considering: $local_part
@@ -678,14 +714,18 @@ CALLER in "CALLER"?
  ├──expanding: /non-exist/$local_part
  ╰─────result: /non-exist/CALLER
             ╰──(tainted)
+try option set
 calling r1 router
 r1 router called for CALLER@test.ex
   domain = test.ex
+try option transport
 set transport t1
 queued for t1 transport: local_part = CALLER
 domain = test.ex
   errors_to=NULL
   domain_data=NULL local_part_data=CALLER
+try option unseen
+try option unseen
 routed by r1 router
   envelope to: CALLER@test.ex
   transport: t1
@@ -723,6 +763,9 @@ search_tidyup called
  returned from EXIM_DBOPEN: (nil)
  failed to open DB file TESTSUITE/spool/db/retry: No such file or directory
 no retry data available
+try option max_parallel
+try option return_path
+try option home_directory
  ╭considering: /non-exist/$local_part
  ├───────text: /non-exist/
  ├considering: $local_part
@@ -731,7 +774,13 @@ no retry data available
  ├──expanding: /non-exist/$local_part
  ╰─────result: /non-exist/usery
             ╰──(tainted)
+try option current_directory
 search_tidyup called
+try option quota
+try option quota_filecount
+try option quota_warn_threshold
+try option mailbox_size
+try option mailbox_filecount
 changed uid/gid: local delivery to TESTSUITE/test-mail/junk <TESTSUITE/test-mail/junk> transport=ft1
   uid=CALLER_UID gid=CALLER_GID pid=p1236
   home=/non-exist/usery current=/
@@ -752,6 +801,7 @@ hitch name: TESTSUITE/test-mail/junk.lock.test.ex.dddddddd.pppppppp
 lock file created
 mailbox TESTSUITE/test-mail/junk is locked
 writing to file TESTSUITE/test-mail/junk
+try option message_prefix
  ╭considering: From ${if def:return_path{$return_path}{MAILER-DAEMON}} ${tod_bsdinbox}
  
  ├───────text: From 
@@ -795,6 +845,7 @@ writing to file TESTSUITE/test-mail/junk
 writing data block fd=dddd size=sss timeout=0
 cannot use sendfile for body: spoolfile not wireformat
 writing data block fd=dddd size=sss timeout=0
+try option message_suffix
 writing data block fd=dddd size=sss timeout=0
 appendfile yields 0 with errno=dd more_errno=dd
 search_tidyup called
@@ -812,7 +863,16 @@ LOG: MAIN
  returned from EXIM_DBOPEN: (nil)
  failed to open DB file TESTSUITE/spool/db/retry: No such file or directory
 no retry data available
+try option max_parallel
+try option return_path
+try option home_directory
+try option current_directory
 search_tidyup called
+try option quota
+try option quota_filecount
+try option quota_warn_threshold
+try option mailbox_size
+try option mailbox_filecount
 changed uid/gid: local delivery to TESTSUITE/test-mail/junk <TESTSUITE/test-mail/junk> transport=ft1
   uid=CALLER_UID gid=CALLER_GID pid=p1237
   home=/non-exist/CALLER current=/
@@ -833,6 +893,7 @@ hitch name: TESTSUITE/test-mail/junk.lock.test.ex.dddddddd.pppppppp
 lock file created
 mailbox TESTSUITE/test-mail/junk is locked
 writing to file TESTSUITE/test-mail/junk
+try option message_prefix
  ╭considering: From ${if def:return_path{$return_path}{MAILER-DAEMON}} ${tod_bsdinbox}
  
  ├───────text: From 
@@ -876,6 +937,7 @@ writing to file TESTSUITE/test-mail/junk
 writing data block fd=dddd size=sss timeout=0
 cannot use sendfile for body: spoolfile not wireformat
 writing data block fd=dddd size=sss timeout=0
+try option message_suffix
 writing data block fd=dddd size=sss timeout=0
 appendfile yields 0 with errno=dd more_errno=dd
 search_tidyup called
@@ -893,7 +955,16 @@ LOG: MAIN
  returned from EXIM_DBOPEN: (nil)
  failed to open DB file TESTSUITE/spool/db/retry: No such file or directory
 no retry data available
+try option max_parallel
+try option return_path
+try option home_directory
+try option current_directory
 search_tidyup called
+try option quota
+try option quota_filecount
+try option quota_warn_threshold
+try option mailbox_size
+try option mailbox_filecount
 changed uid/gid: local delivery to CALLER <CALLER@test.ex> transport=t1
   uid=CALLER_UID gid=CALLER_GID pid=p1238
   home=/non-exist/CALLER current=/
@@ -919,6 +990,9 @@ LOG: MAIN
  returned from EXIM_DBOPEN: (nil)
  failed to open DB file TESTSUITE/spool/db/retry: No such file or directory
 no retry data available
+try option max_parallel
+try option return_path
+try option home_directory
  ╭considering: /non-exist/$local_part
  ├───────text: /non-exist/
  ├considering: $local_part
@@ -927,7 +1001,13 @@ no retry data available
  ├──expanding: /non-exist/$local_part
  ╰─────result: /non-exist/usery
             ╰──(tainted)
+try option current_directory
 search_tidyup called
+try option quota
+try option quota_filecount
+try option quota_warn_threshold
+try option mailbox_size
+try option mailbox_filecount
 changed uid/gid: local delivery to usery <usery@test.ex> transport=t1
   uid=CALLER_UID gid=CALLER_GID pid=p1239
   home=/non-exist/usery current=/
@@ -953,6 +1033,9 @@ LOG: MAIN
  returned from EXIM_DBOPEN: (nil)
  failed to open DB file TESTSUITE/spool/db/retry: No such file or directory
 no retry data available
+try option max_parallel
+try option return_path
+try option home_directory
  ╭considering: /$local_part
  ├───────text: /
  ├considering: $local_part
@@ -961,7 +1044,13 @@ no retry data available
  ├──expanding: /$local_part
  ╰─────result: /userz
             ╰──(tainted)
+try option current_directory
 search_tidyup called
+try option quota
+try option quota_filecount
+try option quota_warn_threshold
+try option mailbox_size
+try option mailbox_filecount
 changed uid/gid: local delivery to userz <userz@test.ex> transport=t2
   uid=CALLER_UID gid=CALLER_GID pid=p1240
   home=/userz current=/

test/stderr/0544
@@ -4,9 +4,18 @@ environment after trimming:
 adding SSLKEYLOGFILE=TESTSUITE/spool/sslkeys
 configuration file is TESTSUITE/test-config
 admin user
+try option gecos_pattern
+try option gecos_name
+try option unknown_login
+try option smtp_active_hostname
+try option message_size_limit
+try option acl_not_smtp_start
+try option message_id_header_domain
+try option message_id_header_text
  ╭considering: ${tod_full}
  ├──expanding: ${tod_full}
  ╰─────result: Tue, 2 Mar 1999 09:44:33 +0000
+try option received_header_text
  ╭considering: Received: ${if def:sender_rcvhost {from $sender_rcvhost
  	}{${if def:sender_ident {from ${quote_local_part:$sender_ident} }}${if def:sender_helo_name {(helo=$sender_helo_name)
  	}}}}by $primary_hostname ${if def:received_protocol {with $received_protocol }}${if def:tls_in_ver        { ($tls_in_ver)}}${if def:tls_in_cipher_std { tls $tls_in_cipher_std
@@ -337,6 +346,8 @@ admin user
  ╰─────result: Received: from CALLER by the.local.host.name with local (Exim x.yz)
  	(envelope-from <CALLER@test.ex>)
  	id 10HmaX-000000005vi-0000
+try option acl_not_smtp_mime
+try option acl_not_smtp
  ╭considering: ${tod_full}
  ├──expanding: ${tod_full}
  ╰─────result: Tue, 2 Mar 1999 09:44:33 +0000
@@ -351,6 +362,20 @@ configuration file is TESTSUITE/test-config
 trusted user
 admin user
 dropping to exim gid; retaining priv uid
+try option router_home_directory
+try option set
+try option transport
+try option unseen
+try option unseen
+try option router_home_directory
+try option set
+try option transport
+try option unseen
+try option unseen
+try option multi_domain
+try option multi_domain
+try option max_parallel
+try option return_path
  ╭considering: $domain
  ├──────value: domain1.ex
             ╰──(tainted)
@@ -361,6 +386,7 @@ LOG: MAIN
   == userx@domain1.ex R=smarthost T=smtp defer (-1): first-pass only routing due to -odqs, queue_smtp_domains or control=queue
 LOG: MAIN
   == userx@domain2.ex R=smarthost T=smtp defer (-1): first-pass only routing due to -odqs, queue_smtp_domains or control=queue
+try option delay_warning_condition
  ╭considering: ${if or {{ !eq{$h_list-id:$h_list-post:$h_list-subscribe:}{} }{ match{$h_precedence:}{(?i)bulk|list|junk} }{ match{$h_auto-submitted:}{(?i)auto-generated|auto-replied} }} {no}{yes}}
   ╭considering: $h_list-id:$h_list-post:$h_list-subscribe:}{} }{ match{$h_precedence:}{(?i)bulk|list|junk} }{ match{$h_auto-submitted:}{(?i)auto-generated|auto-replied} }} {no}{yes}}
   ├considering: $h_list-post:$h_list-subscribe:}{} }{ match{$h_precedence:}{(?i)bulk|list|junk} }{ match{$h_auto-submitted:}{(?i)auto-generated|auto-replied} }} {no}{yes}}
@@ -418,8 +444,15 @@ environment after trimming:
 adding SSLKEYLOGFILE=TESTSUITE/spool/sslkeys
 configuration file is TESTSUITE/test-config
 admin user
+try option gecos_pattern
+try option gecos_name
+try option unknown_login
+try option smtp_active_hostname
 LOG: smtp_connection MAIN
   SMTP connection from CALLER
+try option message_size_limit
+try option acl_smtp_connect
+try option smtp_banner
  ╭considering: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ├──────value: the.local.host.name
  ├considering:  ESMTP Exim $version_number $tod_full
@@ -432,6 +465,8 @@ LOG: smtp_connection MAIN
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: the.local.host.name ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
+try option spf_smtp_comment_template
+try option acl_smtp_helo
  ╭considering: ${if match_domain {$sender_helo_name}{+dlist}}
   ╭considering: $sender_helo_name}{+dlist}}
   ├──────value: ehlo.domain
@@ -455,6 +490,10 @@ LOG: smtp_connection MAIN
  ├─────result: true
  ├──expanding: ${if match_domain {$sender_helo_name}{+dlist}}
  ╰─────result: true
+try option acl_smtp_etrn
+try option acl_smtp_vrfy
+try option acl_smtp_expn
+try option acl_smtp_mail
  ╭considering: domain=$domain/sender_domain=$sender_address_domain
  ├───────text: domain=
  ├considering: $domain/sender_domain=$sender_address_domain
@@ -467,6 +506,7 @@ LOG: smtp_connection MAIN
  ├──expanding: domain=$domain/sender_domain=$sender_address_domain
  ╰─────result: domain=/sender_domain=sender.domain
             ╰──(tainted)
+try option acl_smtp_rcpt
  ╭considering: domain=$domain/sender_domain=$sender_address_domain
  ├───────text: domain=
  ├considering: $domain/sender_domain=$sender_address_domain
@@ -493,6 +533,7 @@ LOG: smtp_connection MAIN
  ├──expanding: domain=$domain/sender_domain=$sender_address_domain
  ╰─────result: domain=recipient.domain/sender_domain=sender.domain
             ╰──(tainted)
+try option acl_smtp_quit
 LOG: smtp_connection MAIN
   SMTP connection from CALLER D=qqs closed by QUIT
 >>>>>>>>>>>>>>>> Exim pid=p1236 (fresh-exec) terminating with rc=0 >>>>>>>>>>>>>>>>

commit e3090e6b15f8c437d700898cbc58120781be5173

Teststuite: munges needed for buildfarm
Broken by: 90bd3832bc0f

test/runtest
@@ -1064,6 +1064,9 @@ RESET_AFTER_EXTRA_LINE_READ:
     # LIMITS is not always supported by the build
     next if /^limits_advertise_hosts =/;
 
+    # PRDR
+    next if /^hosts_try_prdr = *$/;
+
     # TLS resumption is not always supported by the build
     next if /^tls_resumption_hosts =/;
     next if /^-tls_resumption/;
@@ -1380,6 +1383,9 @@ RESET_AFTER_EXTRA_LINE_READ:
     # so convert them all to "0"
     s%(?<!lsearch)[^ ](?=TESTSUITE/aux-fixed/(?:0414.list[12]|0464.domains)$)%0%;
 
+    # CONTENT_SCAN
+    next if /try option acl_not_smtp_mime$/;
+
     # DISABLE_OCSP
     next if /in hosts_requ(est|ire)_ocsp\? (no|yes)/;
 
@@ -1447,6 +1453,7 @@ RESET_AFTER_EXTRA_LINE_READ:
 
     # Not all platforms build with SPF enabled
     next if /^(spf_conn_init|SPF_dns_exim_new|spf_compile\.c)/;
+    next if /try option spf_smtp_comment_template$/;
 
     # Not all platforms have sendfile support
     next if /^cannot use sendfile for body: no support$/;
@@ -4893,4 +4900,5 @@ Start Exim wrapped by I<valgrind>. (default: don't use valgrind)
 =cut
 
 
+# vi: aw ai sw=2
 # End of runtest script

test/stderr/0092
@@ -127,7 +127,6 @@ test in helo_lookup_domains? no (end of list)
 sender_fullhost = (test) [V4NET.0.0.1]
 sender_rcvhost = [V4NET.0.0.1] (helo=test)
 set_process_info: pppp handling incoming connection from (test) [V4NET.0.0.1]
-try option spf_smtp_comment_template
 try option acl_smtp_helo
 SMTP>> 250 myhost.test.ex Hello test [V4NET.0.0.1]
 SMTP<< mail from:userx@test.ex

test/stderr/0402
@@ -389,7 +389,6 @@ P Received: from CALLER by mail.test.ex with local (Exim x.yz)
 	(envelope-from <CALLER@test.ex>)
 	id 10HmaX-000000005vi-0000;
 	Tue, 2 Mar 1999 09:44:33 +0000
-try option acl_not_smtp_mime
 try option acl_not_smtp
  ╭considering: ${tod_full}
  ├──expanding: ${tod_full}

test/stderr/0544
@@ -346,7 +346,6 @@ try option received_header_text
  ╰─────result: Received: from CALLER by the.local.host.name with local (Exim x.yz)
  	(envelope-from <CALLER@test.ex>)
  	id 10HmaX-000000005vi-0000
-try option acl_not_smtp_mime
 try option acl_not_smtp
  ╭considering: ${tod_full}
  ├──expanding: ${tod_full}
@@ -465,7 +464,6 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: the.local.host.name ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
-try option spf_smtp_comment_template
 try option acl_smtp_helo
  ╭considering: ${if match_domain {$sender_helo_name}{+dlist}}
   ╭considering: $sender_helo_name}{+dlist}}

test/stderr/0620
@@ -25,7 +25,6 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: primaryhostname.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
-try option spf_smtp_comment_template
 try option acl_smtp_helo
 try option acl_smtp_mail
 try option acl_smtp_rcpt
@@ -100,7 +99,6 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: primaryhostname.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
-try option spf_smtp_comment_template
 try option acl_smtp_helo
 try option acl_smtp_mail
 try option acl_smtp_rcpt

test/stderr/0632
@@ -26,7 +26,6 @@ listening on all interfaces (IPv4) port PORT_D
 pid written to TESTSUITE/spool/exim-daemon.pid
 LOG: MAIN
   exim x.yz daemon started: pid=p1234, no queue runs, listening for SMTP on port PORT_D
-try option spf_smtp_comment_template
 daemon running with uid=EXIM_UID gid=EXIM_GID euid=EXIM_UID egid=EXIM_GID
 Listening...
 Connection request from 127.0.0.1 port sssss

test/stderr/5410
@@ -26,10 +26,9 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: myhost.test.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
+try option acl_smtp_helo
  list element: *
   in limits_advertise_hosts? yes (matched "*")
-try option spf_smtp_comment_template
-try option acl_smtp_helo
  in dsn_advertise_hosts? no (option unset)
 try option acl_smtp_etrn
 try option acl_smtp_vrfy
@@ -82,6 +81,9 @@ domain.com in "! +local_domains"?
    domain.com in "test.ex : *.test.ex"? no (end of list)
   end sublist local_domains
 domain.com in "! +local_domains"? yes (end of list)
+try option router_home_directory
+try option set
+processing address_data
  ╭considering: $local_part
  ├──────value: userx
             ╰──(tainted)
@@ -91,6 +93,8 @@ domain.com in "! +local_domains"? yes (end of list)
 domain.com in "*"?
  list element: *
  domain.com in "*"? yes (matched "*")
+try option transport
+try option unseen
 try option unseen
 ----------- end verify ------------
 accept: condition test succeeded in ACL "cutthrough"
@@ -105,6 +109,9 @@ domain.com in "! +local_domains"?
    domain.com in "test.ex : *.test.ex"? no (end of list)
   end sublist local_domains
 domain.com in "! +local_domains"? yes (end of list)
+try option router_home_directory
+try option set
+processing address_data
  ╭considering: $local_part
  ├──────value: userx
             ╰──(tainted)
@@ -114,6 +121,8 @@ domain.com in "! +local_domains"? yes (end of list)
 domain.com in "*"?
  list element: *
  domain.com in "*"? yes (matched "*")
+try option transport
+try option unseen
 try option unseen
 try option interface
 Connecting to 127.0.0.1 [127.0.0.1]:PORT_D from ip4.ip4.ip4.ip4 ... try option dscp
@@ -678,10 +687,9 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: myhost.test.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
+try option acl_smtp_helo
  list element: *
   in limits_advertise_hosts? yes (matched "*")
-try option spf_smtp_comment_template
-try option acl_smtp_helo
  in dsn_advertise_hosts? no (option unset)
 try option acl_smtp_etrn
 try option acl_smtp_vrfy
@@ -734,6 +742,9 @@ domain.com in "! +local_domains"?
    domain.com in "test.ex : *.test.ex"? no (end of list)
   end sublist local_domains
 domain.com in "! +local_domains"? yes (end of list)
+try option router_home_directory
+try option set
+processing address_data
  ╭considering: $local_part
  ├──────value: usery
             ╰──(tainted)
@@ -743,6 +754,8 @@ domain.com in "! +local_domains"? yes (end of list)
 domain.com in "*"?
  list element: *
  domain.com in "*"? yes (matched "*")
+try option transport
+try option unseen
 try option unseen
 ----------- end verify ------------
 accept: condition test succeeded in ACL "cutthrough"
@@ -757,6 +770,9 @@ domain.com in "! +local_domains"?
    domain.com in "test.ex : *.test.ex"? no (end of list)
   end sublist local_domains
 domain.com in "! +local_domains"? yes (end of list)
+try option router_home_directory
+try option set
+processing address_data
  ╭considering: $local_part
  ├──────value: usery
             ╰──(tainted)
@@ -766,6 +782,8 @@ domain.com in "! +local_domains"? yes (end of list)
 domain.com in "*"?
  list element: *
  domain.com in "*"? yes (matched "*")
+try option transport
+try option unseen
 try option unseen
 try option interface
 Connecting to 127.0.0.1 [127.0.0.1]:PORT_D from ip4.ip4.ip4.ip4 ... try option dscp
@@ -1280,10 +1298,9 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: myhost.test.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
+try option acl_smtp_helo
  list element: *
   in limits_advertise_hosts? yes (matched "*")
-try option spf_smtp_comment_template
-try option acl_smtp_helo
  in dsn_advertise_hosts? no (option unset)
 try option acl_smtp_etrn
 try option acl_smtp_vrfy
@@ -1336,6 +1353,9 @@ domain.com in "! +local_domains"?
    domain.com in "test.ex : *.test.ex"? no (end of list)
   end sublist local_domains
 domain.com in "! +local_domains"? yes (end of list)
+try option router_home_directory
+try option set
+processing address_data
  ╭considering: $local_part
  ├──────value: usery
             ╰──(tainted)
@@ -1345,6 +1365,8 @@ domain.com in "! +local_domains"? yes (end of list)
 domain.com in "*"?
  list element: *
  domain.com in "*"? yes (matched "*")
+try option transport
+try option unseen
 try option unseen
 ----------- end verify ------------
 accept: condition test succeeded in ACL "cutthrough"
@@ -1359,6 +1381,9 @@ domain.com in "! +local_domains"?
    domain.com in "test.ex : *.test.ex"? no (end of list)
   end sublist local_domains
 domain.com in "! +local_domains"? yes (end of list)
+try option router_home_directory
+try option set
+processing address_data
  ╭considering: $local_part
  ├──────value: usery
             ╰──(tainted)
@@ -1368,6 +1393,8 @@ domain.com in "! +local_domains"? yes (end of list)
 domain.com in "*"?
  list element: *
  domain.com in "*"? yes (matched "*")
+try option transport
+try option unseen
 try option unseen
 try option interface
 Connecting to 127.0.0.1 [127.0.0.1]:PORT_D from ip4.ip4.ip4.ip4 ... try option dscp

test/stderr/5420
@@ -26,10 +26,9 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: myhost.test.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
+try option acl_smtp_helo
  list element: *
   in limits_advertise_hosts? yes (matched "*")
-try option spf_smtp_comment_template
-try option acl_smtp_helo
  in dsn_advertise_hosts? no (option unset)
 try option acl_smtp_etrn
 try option acl_smtp_vrfy
@@ -688,10 +687,9 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: myhost.test.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
+try option acl_smtp_helo
  list element: *
   in limits_advertise_hosts? yes (matched "*")
-try option spf_smtp_comment_template
-try option acl_smtp_helo
  in dsn_advertise_hosts? no (option unset)
 try option acl_smtp_etrn
 try option acl_smtp_vrfy
@@ -1300,10 +1298,9 @@ try option smtp_banner
  ├──────value: Tue, 2 Mar 1999 09:44:33 +0000
  ├──expanding: $smtp_active_hostname ESMTP Exim $version_number $tod_full
  ╰─────result: myhost.test.ex ESMTP Exim x.yz Tue, 2 Mar 1999 09:44:33 +0000
+try option acl_smtp_helo
  list element: *
   in limits_advertise_hosts? yes (matched "*")
-try option spf_smtp_comment_template
-try option acl_smtp_helo
  in dsn_advertise_hosts? no (option unset)
 try option acl_smtp_etrn
 try option acl_smtp_vrfy