2bb42e2899db219f4a598530b43e06b1b977a81e
Jeremy Harris <jgh146exb@wizmail.org>
2024-09-01 20:15:21 +0100

    Add dummy file to miscmods archive to ensure non-empty

---
+++ src/src/miscmods/dummy.c
@@ -0,0 +1,11 @@
+/*************************************************
+*     Exim - an Internet mail transport agent    *
+*************************************************/
+
+/* Copyright (c) The Exim Maintainers 2024 */
+/* See the file NOTICE for conditions of use and distribution. */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/* Dummy source just so that miscmods.a is never empty */
+
+int dummy;
