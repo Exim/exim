34d4ce03260bf7b6ded8a5f61e7b02601461f9b4
divinity76 <hans@loltek.net>
2024-10-12 16:15:14 +0200

    sort alphabetically
    https://github.com/Exim/exim/pull/94#discussion_r1797707572

--- src/src/readconf.c
+++ src/src/readconf.c
@@ -168,7 +168,6 @@ static optionlist optionlist_config[] = {
 #ifndef DISABLE_TLS
   { "gnutls_allow_auto_pkcs11", opt_bool,        {&gnutls_allow_auto_pkcs11} },
   { "gnutls_compat_mode",       opt_bool,        {&gnutls_compat_mode} },
-  { "tls_ignore_missing_close_notify", opt_bool, {&tls_ignore_missing_close_notify} },
 #endif
   { "header_line_maxsize",      opt_int,         {&header_line_maxsize} },
   { "header_maxsize",           opt_int,         {&header_maxsize} },
@@ -384,6 +383,7 @@ static optionlist optionlist_config[] = {
   { "tls_dh_max_bits",          opt_int,         {&tls_dh_max_bits} },
   { "tls_dhparam",              opt_stringptr,   {&tls_dhparam} },
   { "tls_eccurve",              opt_stringptr,   {&tls_eccurve} },
+  { "tls_ignore_missing_close_notify", opt_bool, {&tls_ignore_missing_close_notify} },
 # ifndef DISABLE_OCSP
   { "tls_ocsp_file",            opt_stringptr,   {&tls_ocsp_file} },
 # endif
