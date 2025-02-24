5440fd04d2a2be2e76c0740191ab45fafe480c41
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2025-02-24 23:29:47 +0100

    doc: comment the prototypes

--- src/src/hintsdb.h
+++ src/src/hintsdb.h
@@ -101,9 +101,52 @@ the default is the NDBM interface (which seems to be a wrapper for GDBM) */
 # include "hintsdb/hints_ndbm.h"
 #endif /* !USE_GDBM */
 
+/* All hints db backends must provide these functions */
 
+/* open/close
+The actual exim_dbopen*, exim_dbclose* functions are below, they wrap these here. */
 
+/* exim_dbopen returns a pointer to an EXIM_DB, NULL if failed */
+EXIM_DB * exim_dbopen__(const uschar * name, const uschar * dirname, int flags, unsigned mode);
+EXIM_DB * exim_dbopen_multi__(const uschar * name, const uschar * dirname, int flags, unsigned mode);
+void exim_dbclose__(EXIM_DB * dbp);
+void exim_dbclose_multi__(EXIM_DB * dbp);
 
+/* accessors: single key */
+/* exim_dbput returns nothing useful, it assumes replace mode */
+int exim_dbput(EXIM_DB * dbp, EXIM_DATUM * key, EXIM_DATUM * data);
+
+/* exim_dbputb returns EXIM_DBPUTB_OK if successful, EXIM_DBPUTB_DUP otherwise. The
+the actual values of EXIM_DBPUTB_OK and EXIM_DBPUT_DUP depend on the backend */
+int exim_dbputb(EXIM_DB * dbp, EXIM_DATUM * key, EXIM_DATUM * data);
+
+/* exim_dbdel */
+int exim_dbdel(EXIM_DB * dbp, EXIM_DATUM * key);
+
+/* exim_dbget returns TRUE if successful, FALSE otherwise */
+BOOL exim_dbget(EXIM_DB * dbp, EXIM_DATUM * key, EXIM_DATUM * res);
+
+/* accessors: enumeration */
+/* exim_dbcreate_cursor returns a valid pointer to a EXIM_CURSOR or NULL */
+EXIM_CURSOR * exim_dbcreate_cursor(EXIM_DB * dbp);
+
+/* exim_dbdelete_cursor frees the cursor */
+void exim_dbdelete_cursor(EXIM_CURSOR * cursor);
+
+/* exim_dbscan returns TRUE if data is returned, FALSE at end */
+BOOL exim_dbscan(EXIM_DB * dbp, EXIM_DATUM * key, EXIM_DATUM * res, BOOL first, EXIM_CURSOR * cursor);
+
+/* transactions */
+BOOL exim_dbtransaction_start(EXIM_DB * dbp);
+void exim_dbtransaction_commit(EXIM_DB * dbp);
+
+/* datum */
+void exim_datum_init(EXIM_DATUM * dp);
+void exim_datum_free(EXIM_DATUM * dp);
+void exim_datum_data_set(EXIM_DATUM * dp, void * s);
+void exim_datum_size_set(EXIM_DATUM * dp, unsigned n);
+uschar * exim_datum_data_get(EXIM_DATUM * dp);
+unsigned exim_datum_size_get(EXIM_DATUM * dp);
 
 
 /* Wrappers for open/close with debug tracing */
@@ -165,6 +208,7 @@ exim_dbclose(EXIM_DB * dbp)
 DEBUG(D_hints_lookup) debug_printf_indent("EXIM_DBCLOSE(%p)\n", dbp);
 exim_dbclose__(dbp);
 }
+
 static inline void
 exim_dbclose_multi(EXIM_DB * dbp)
 {
