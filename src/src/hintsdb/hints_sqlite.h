c2059346fd267cf21b612ec34383aa27da804704
Jeremy Harris <jgh146exb@wizmail.org>
2025-11-02 19:58:29 +0000

    Build: quieten sqlite-hints -Wunused-function build
    (cherry picked from commit b93ee3883ef8a11c440c5519812f3cb6c074a02f)
    
    This commit was lost during sqlite refactoring.

--- src/src/hintsdb/hints_sqlite.h
+++ src/src/hintsdb/hints_sqlite.h
@@ -292,15 +292,13 @@ exim_dbclose_multi__(dbp);
 static inline uschar *
 exim_datum_data_get(EXIM_DATUM * dp)
 { return US dp->data; }
-
-static void
+static inline void
 exim_datum_data_set(EXIM_DATUM * dp, void * s)
 { dp->data = s; }
 
-static unsigned
+static inline unsigned
 exim_datum_size_get(EXIM_DATUM * dp)
 { return dp->len; }
-
 static inline void
 exim_datum_size_set(EXIM_DATUM * dp, unsigned n)
 { dp->len = n; }
